487b6c24a30288c0e8a07dbabe20175f9b3f01d8
Prajeeth777 <prajeethkumar777@gmail.com>
2025-01-12 20:23:35 +0900

    Update Activity 4.py

--- Activity 4.py
+++ Activity 4.py
@@ -1,1 +1,18 @@
-class
+class Parrot:
+  species="Bird"
+
+  def _init_(self,name,age):
+
+    self.name=name
+    self.age=age
+
+def sing(self,song):
+  return "{} sings {} song ".format(self.name,song)
+
+def dance(self):
+  return self.name," is dancing now"
+
+p1=Parrot("Rio",2)
+print(p1.name, "is a ",p1.species," and is ",p1.age," years old")
+print(p1.sing("happy"))
+print(p1.dance())
