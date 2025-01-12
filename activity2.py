5462f436dd901c976093dfd37aaff8b529be02e9
Prajeeth777 <prajeethkumar777@gmail.com>
2025-01-12 19:54:48 +0900

    Create activity2.py

---
+++ activity2.py
@@ -0,0 +1,12 @@
+class student:
+  grade=10
+  name="James"
+
+def show(self):
+  print("Welcome to my first class method")
+def display(self):
+  print("Name of the student is ",self.name," and grade is ",self.grade)
+
+st1=student()
+st1.show()
+st1.display()
